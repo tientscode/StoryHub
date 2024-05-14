ef1cfce52a21ad5538e5232aa00659bcaaab892f
tientscode <tienit.tscode@gmail.com>
2024-05-14 07:56:49 +0700

    StoryHub_database_14/05/2024

--- src/main/java/com/tscode/java5/database/connect_database.java
+++ src/main/java/com/tscode/java5/database/connect_database.java
@@ -1,4 +1,19 @@
-package com.tscode.java5;
+package com.tscode.java5.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class connect_database {
+
+    public static void main(String[] args) {
+        var url="jdbc:mysql://localhost:3306/StoryHub";
+        var user="root";
+        var password="";
+        try(Connection conn= DriverManager.getConnection(url, user, password)) {
+            System.out.println(conn.getCatalog());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
